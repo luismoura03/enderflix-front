b45f0810344a6ffdabb1a27234eb606b246e3a21
luismoura03 <gui.batidao3@gmail.com>
2025-01-21 21:38:59 -0300

    :construction: implementing toolbar component

--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -8,16 +8,12 @@
     <v-toolbar extended>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title>EnderFlix</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
       </v-btn>
 
       <v-btn icon>
